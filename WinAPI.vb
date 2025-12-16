c6a2c2c9a069d26f57f7b357d06ea09edde42b60
YodeSkye <YodeSkye@outlook.com>
2025-12-16 15:04:18 -0500

    WinAPI CLipboard Updates

--- WinAPI.vb
+++ WinAPI.vb
@@ -58,6 +58,14 @@ Public Class WinAPI
     Public Const WM_CHANGECBCHAIN As Integer = 781 '&H30D
     Public Const WM_DRAWCLIPBOARD As Integer = 776 '&H308
     Public Const WM_CLIPBOARDUPDATE As Integer = &H31D
+    Public Const GMEM_MOVEABLE As UInteger = &H2
+    Public Const CF_TEXT As UInteger = 1
+    Public Const CF_BITMAP As UInteger = 2
+    Public Const CF_OEMTEXT As UInteger = 7
+    Public Const CF_UNICODETEXT As UInteger = 13
+    Public Const CF_HDROP As UInteger = 15
+    Public Const CF_DIB As UInteger = 8
+    Public Const CF_DIBV5 As UInteger = 17
     <DllImport("user32.dll", SetLastError:=True)>
     Public Shared Function AddClipboardFormatListener(hwnd As IntPtr) As Boolean
     End Function
@@ -69,6 +77,48 @@ Public Class WinAPI
     End Function
     <DllImport("user32.dll", SetLastError:=True)>
     Public Shared Function GetClipboardSequenceNumber() As UInteger
+    End Function
+    <DllImport("user32.dll", SetLastError:=True)>
+    Public Shared Function OpenClipboard(hwnd As IntPtr) As Boolean
+    End Function
+    <DllImport("user32.dll", SetLastError:=True)>
+    Public Shared Function CloseClipboard() As Boolean
+    End Function
+    <DllImport("user32.dll", SetLastError:=True)>
+    Public Shared Function EmptyClipboard() As Boolean
+    End Function
+    <DllImport("user32.dll", SetLastError:=True)>
+    Public Shared Function EnumClipboardFormats(format As UInteger) As UInteger
+    End Function
+    <DllImport("user32.dll", SetLastError:=True)>
+    Public Shared Function GetClipboardData(uFormat As UInteger) As IntPtr
+    End Function
+    <DllImport("user32.dll", SetLastError:=True)>
+    Public Shared Function SetClipboardData(uFormat As UInteger, hMem As IntPtr) As IntPtr
+    End Function
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Public Shared Function GlobalAlloc(uFlags As UInteger, dwBytes As UIntPtr) As IntPtr
+    End Function
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Public Shared Function GlobalLock(hMem As IntPtr) As IntPtr
+    End Function
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Public Shared Function GlobalUnlock(hMem As IntPtr) As Boolean
+    End Function
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Public Shared Function GlobalSize(hMem As IntPtr) As Integer
+    End Function
+    <DllImport("user32.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
+    Public Shared Function GetClipboardFormatName(format As UInteger, lpszFormatName As System.Text.StringBuilder, cchMaxCount As Integer) As Integer
+    End Function
+    <DllImport("user32.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
+    Public Shared Function RegisterClipboardFormat(lpszFormat As String) As UInteger
+    End Function
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Public Shared Function GlobalFlags(hMem As IntPtr) As Integer
+    End Function
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Public Shared Function GlobalFree(hMem As IntPtr) As IntPtr
     End Function
 
     'Window Functions 'Get Information About & Change Attributes Of A Window
